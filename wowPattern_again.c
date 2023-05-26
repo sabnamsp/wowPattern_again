8fa8a8347c5a54fd797a5f24faef1f3fe06241cd
Sabnam Parvin <114932621+sabnamsp@users.noreply.github.com>
2023-05-26 11:28:21 +0600

    Update wowPattern_again.c

--- wowPattern_again.c
+++ wowPattern_again.c
@@ -1,1 +1,32 @@
+#include<stdio.h>
 
+int main()
+{
+    int n,a,b;
+    scanf("%d",&n);
+    a=n-1;
+    b=1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=a;j++)
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=b;j++)
+        {
+            if(i%2== 0)
+            {
+                printf("*");
+            }
+            else 
+            {
+                printf("^");
+            }
+
+        }
+        a--;
+        b=b+2;
+        printf("\n");
+    }
+    return 0;
+}
